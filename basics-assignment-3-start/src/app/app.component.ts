6ad7f2df66d211594818418bb07d196125707164
rtrydev <r.kulka44@gmail.com>
2021-12-25 23:54:02 +0100

    complete assignment

--- basics-assignment-3-start/src/app/app.component.ts
+++ basics-assignment-3-start/src/app/app.component.ts
@@ -6,4 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  secretPasswordVisible = false;
+  clickTimes: string[] = [];
+  clickCount = 0;
+
+  togglePasswordVisibility() {
+    this.secretPasswordVisible = !this.secretPasswordVisible;
+    this.clickTimes.push(Date());
+    this.clickCount++;
+  }
+
+  getBackgroundColor(i: number) {
+    return i > 3 ? 'blue' : 'white';
+  }
 }
